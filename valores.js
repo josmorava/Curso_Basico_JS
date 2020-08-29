a100e8237204fd165a9858305a3e25a312c31397
josmorava <josemorales-1234@hotmail.es>
2020-08-28 21:07:38 -0400

    Clase 3, valores

---
+++ valores.js
@@ -0,0 +1,19 @@
+"Valores numéricos"
+40 
+
+" Valor string José Morales"
+
+"Valores Booleanos"
+true 
+false
+
+
+"valores vacios, primitivos"
+null 
+undefined 
+
+"Valores tipos objetos, no primitivos--arrais,objeto"
+[1,2,3]
+{nombre:"JoseMorales"}
+
+"Palabra -> typeof dentro de la consola del navegador para saber que tipo de valor es cada elemnto que escribamos"
