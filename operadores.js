abaa6ab178e698708e4f9d28f4bdb3e06bc747c5
josmorava <josemorales-1234@hotmail.es>
2020-09-01 18:39:46 -0400

    Clase 9, operadores

---
+++ operadores.js
@@ -0,0 +1,26 @@
+//Operadores matemáticos, binarios porque son entre dos valores
+3 +3
+4-2
+2*2
+200/2
+"Jose" + "Morales"
+
+//Operadore de negación
+!false
+
+//Operadores de asinagcion,comparacion,igualacion
+var a = 1;
+3 == "3";
+3=== "3";
+5>4
+4<1
+4>=7
+//Si ambos valores son iguales
+a && b //que las dos sean verdad
+
+//Operador de negacion
+a || b //Si alguna de las dos es verdad, cualquiera de las dos puede ser verdad
+
+//Operador de incrementacion
+var edad = 20;
+edad ++
