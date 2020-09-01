8499a06cfc9483818bfa1aed26e6b8f9d7eed05a
josmorava <josemorales-1234@hotmail.es>
2020-09-01 00:22:27 -0400

    Clase 4, variables

---
+++ variables.js
@@ -0,0 +1,22 @@
+//Así se hace un comentario en js lol
+
+//Una variable es la representacion de un espacio guardado en memoria, puede tener diferentes tipos
+
+var nombre= "Jose";
+
+//estados de las  variables:
+    //1-> DECLARAR, reservar espacio
+    var edad;
+
+    //2->INCIALIZAR, utilizar.
+    edad = 20;
+    
+    //variables de arrails
+     var elementos = ["computadora", "celular"];
+
+     //variable tipo objeto
+     var persona = {
+         nombre : "Jose",
+         edad: 20
+     }
+     persona;
